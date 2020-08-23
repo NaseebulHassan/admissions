e5f7f9b361aa6f1f30727b3169b10ffec0842a3e
NaseebulHassan <naseebulhassan4@gmail.com>
2020-08-23 23:31:54 +0500

    cmit

--- iuic_sic.sql
+++ iuic_sic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 08, 2020 at 10:17 PM
+-- Generation Time: Aug 23, 2020 at 05:37 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -144,6 +144,18 @@ CREATE TABLE `student__academics` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `student__academics`
+--
+
+INSERT INTO `student__academics` (`id`, `student_id`, `qualification`, `board_university`, `year`, `division`, `grade`, `obtain_marks`, `total_marks`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Matric', 'hsjdfhh', '2015', '1st', 'A+', '852', '4568', NULL, NULL),
+(2, 1, 'Matric', 'hsjdfhh', '2015', '1st', 'A+', '852', '4568', NULL, NULL),
+(3, 1, 'Matric', 'hsjdfhh', '2015', '1st', 'A+', '852', '4568', NULL, NULL),
+(4, 1, 'Matric', 'hsjdfhh', '2015', '1st', 'A+', '852', '4568', NULL, NULL),
+(5, 2, 'Matric', 'Multan', '2013', '1st', 'A+', '852', '1050', NULL, NULL),
+(6, 2, 'FSC', 'Federal', '2015', '2nd', 'B', '625', '1100', NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -192,6 +204,15 @@ CREATE TABLE `student__basicinfos` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `student__basicinfos`
+--
+
+INSERT INTO `student__basicinfos` (`id`, `student_id`, `student_name`, `student_cnic`, `father_name`, `father_cnic`, `guardian_name`, `guardian_cnic`, `gender`, `martial_status`, `domicile_province`, `domicile_district`, `date_of_birth`, `place_of_birth`, `nationality1`, `nationality2`, `present_address`, `present_tel`, `present_mobile`, `permanent_address`, `perm_tel`, `perm_mobile`, `student_email`, `father_occupation`, `fbusiness_address`, `father_tel`, `father_mobile`, `father_email`, `applicant_designation`, `organisation_name`, `disability`, `special_arrangement`, `emergency_name`, `relation_applicant`, `em_telephone`, `em_mobile`, `em_address`, `created_at`, `updated_at`) VALUES
+(5, 1, 'sfadgf', '43256-7364764-3', 'dfgfdhgf', '43262-8476238-6', 'ghggfhh', '36218-7467823-6', 'Male', 'single', 'ashhfh', 'fhsdhgf', '2020-08-05', 'haskdaskg', 'hghfgshgf', NULL, 'sjkfjsafhsaj', '4335-3465475', '4333-5345345', 'jjafgdhfsdjfhagdajf', '3232-5325346', '4353-4534543', 'naseeb@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-08-20 04:18:52', '2020-08-20 04:18:52'),
+(6, 1, 'cjfhjksfa', '36876-4783265-8', 'jkfgsdjgfk', '72648-7326863-2', 'kgksgfkgdsk', '32687-3265873-2', 'Male', 'single', 'gdsjfgsjdghja', 'hjsgfsdhgfdh', '2020-08-06', 'fjdsgjfajdf', 'gyufegygrfwueyf', NULL, 'fhsagfjhasgfh', '2178-6478214', '7364-6274563', 'jhshfgshhagfhs', '7364-8723646', '2342-3874623', 'naseeb@gmail.com', 'asbjhasfhas', 'ajsfkjsagfj', '3274672146162', '732268476', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-08-20 05:26:44', '2020-08-20 05:26:44'),
+(7, 2, 'Hassan', '23834-5327495-3', 'Ahmed', '23894-8237589-2', 'Umer', '92864-8236482-3', 'Male', 'single', 'Punjab', 'Rawalpindi', '2020-08-05', 'Rawalpindi', 'Pakistani', NULL, 'House Cd-621, st# 123, mohalla multani , cantt rawlpindi', '3243-2432433', '2344-3243242', 'same as above', '3242-3432432', '2342-3424324', 'hassan@gmail.com', 'Farmer', 'Farmer', '231323123123', '432423432432', NULL, NULL, NULL, NULL, NULL, 'ALi', 'brother', '3213232131', '23243242342324', 'same as above', '2020-08-21 01:36:03', '2020-08-21 01:36:03');
+
 -- --------------------------------------------------------
 
 --
@@ -224,6 +245,14 @@ CREATE TABLE `student__programs` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `student__programs`
+--
+
+INSERT INTO `student__programs` (`id`, `student_id`, `campus`, `registration_no`, `pre_program`, `reason_leaving`, `institution_attended`, `cgpa`, `credit_hours`, `program`, `tr_reason_leaving`, `awaiting_result`, `board_university`, `roll_number`, `year`, `disciplinary_issue`, `disciplinary_details`, `how_did_come`, `student_status`, `program1`, `program2`, `program3`, `created_at`, `updated_at`) VALUES
+(1, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Result Awaiting', 'BS(ECO)', NULL, NULL, '2020-08-20 04:22:03', '2020-08-20 04:22:03'),
+(2, 2, 'Islamabad', '556', 'BS Computer science', 'Left the city', 'PMAS', '3.5', '120', 'BS CS', 'Left the city', NULL, NULL, NULL, NULL, 'no', NULL, 'Internet', 'Result Awaiting', 'BS(CS)', 'None', 'None', '2020-08-21 01:39:27', '2020-08-21 01:39:27');
 
 -- --------------------------------------------------------
 
@@ -268,8 +297,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Naseeb ul Hassan', 'naseeb@gmail.com', NULL, '$2y$10$ktsUvA.XqzsMWA.oo5KlcOa6nyRrPwKWa1X8bMeFd0EjM9w2jQdG.', NULL, '2020-08-08 14:58:43', '2020-08-08 14:58:43'),
-(2, 'Hassan', 'hassan@gmail.com', NULL, 'hassan123', NULL, '2020-08-08 15:11:02', '2020-08-08 15:11:02');
+(1, 'Naseb ul Hassan', 'naseeb@gmail.com', NULL, '$2y$10$mfnYVbxees9PBPR7tow15O.7AGaVx.8mEMLI6r6AnYI1fIeUKoZQa', NULL, '2020-08-20 01:33:32', '2020-08-20 01:33:32'),
+(2, 'Hassan', 'hassan@gmail.com', NULL, '$2y$10$AGrts6ZWBTcu6ROd2kudQeEmOGaKeGC6n9ykPizSjbIuUU3esuXqy', NULL, '2020-08-21 01:31:28', '2020-08-21 01:31:28'),
+(3, 'admin', 'admin@gmail.com', NULL, '$2y$10$OwVbXW3j2K/ZAvJLV2SLu.hNRrZpcxJ9zsxaGBPESwR1udNTRM2U2', NULL, '2020-08-22 00:00:46', '2020-08-22 00:00:46');
 
 --
 -- Indexes for dumped tables
@@ -358,19 +388,19 @@ ALTER TABLE `onlineapplicants`
 -- AUTO_INCREMENT for table `student__academics`
 --
 ALTER TABLE `student__academics`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `student__basicinfos`
 --
 ALTER TABLE `student__basicinfos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `student__programs`
 --
 ALTER TABLE `student__programs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `uploads`
@@ -382,7 +412,7 @@ ALTER TABLE `uploads`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
